f80e2c11bcdae737eaf5e28621b46d086cb30fb4
Abhas Gaur <97803728+Abhas009@users.noreply.github.com>
2024-03-05 23:25:42 +0530

    Update arrayIterations.js

--- Callback/arrayIterations.js
+++ Callback/arrayIterations.js
@@ -1,9 +1,8 @@
 const numbers = [1, 2, 3, 4, 5];
-
+//The forEach method in JavaScript is used to iterate over an array and execute a callback function for each element in the array. Here's how it works:
 // Using forEach with a callback function
 numbers.forEach(function(number) {
     console.log(number * 2);
 });
 
 
-//forEach iterate array
